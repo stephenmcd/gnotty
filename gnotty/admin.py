1b7fe2d7a6ae828489d1d950222bf54f25105328
Stephen McDonald <steve@jupo.org>
2012-06-12 08:24:36 +1000

    Fill out the message logging admin.

--- gnotty/admin.py
+++ gnotty/admin.py
@@ -4,4 +4,13 @@ from django.contrib import admin
 from gnotty.models import IRCMessage
 
 
-admin.site.register(IRCMessage)
+class IRCMessageAdmin(admin.ModelAdmin):
+
+    list_display = ("message_time", "server", "channel", "nickname",
+                    "short_message")
+    list_filter = ("server", "channel", "nickname")
+    search_fields = ("channel", "nickname", "message")
+    date_hierarchy = "message_time"
+
+
+admin.site.register(IRCMessage, IRCMessageAdmin)
--- gnotty/models.py
+++ gnotty/models.py
@@ -1,18 +1,31 @@
 
 from django.db import models
 from django.utils.timezone import now
+from django.utils.translation import ugettext_lazy as _
 
 
 class IRCMessage(models.Model):
+    """
+    An IRC message logged by the logging client from the
+    ``run_gnotty`` management command.
+    """
 
-    nickname = models.CharField(max_length=100)
-    message = models.TextField()
-    server = models.CharField(max_length=100)
-    channel = models.CharField(max_length=100)
-    message_time = models.DateTimeField()
+    nickname = models.CharField(_("Nickname"), max_length=100)
+    message = models.TextField(_("Message"))
+    server = models.CharField(_("Server"), max_length=100)
+    channel = models.CharField(_("Channel"), max_length=100)
+    message_time = models.DateTimeField(_("Time"), auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.message_time = now()
-        super(IRCMessage, self).save(*args, **kwargs)
+    class Meta:
+        verbose_name = _("Message")
+        verbose_name_plural = _("Messages")
+        ordering = ("message_time",)
 
+    def __unicode__(self):
+        return "[%s] %s%s %s: %s" % (self.message_time, self.server,
+                                     self.channel, self.nickname,
+                                     self.short_message())
+
+    def short_message(self):
+        return self.message[:50]
+    short_message.short_description = _("Message")
