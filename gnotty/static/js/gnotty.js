ac099c60e47f01d1ff22844fba10b5cc793eb87f
Stephen McDonald <steve@jupo.org>
2012-06-13 21:17:07 +1000

    Separate socket and UI JavaScript.

---
+++ gnotty/static/js/gnotty.js
@@ -0,0 +1,31 @@
+
+var Gnotty = function(options) {
+
+    var self = this;
+    var socket = io.connect(options.httpHost + ':' + options.httpPort)
+
+    self.message = function(message) {
+        socket.emit('message', message);
+    };
+
+    socket.on('connect', function() {
+        socket.emit('start', options.ircHost, options.ircPort,
+                             options.ircChannel, options.ircNickname);
+        if (self.onConnect) {
+            self.onConnect();
+        }
+    });
+
+    socket.on('nicknames', function(nicknames) {
+        if (self.onNicknames) {
+            self.onNicknames(nicknames);
+        }
+    });
+
+    socket.on('message', function(nickname, message) {
+        if (self.onMessage) {
+            self.onMessage({nickname: nickname, message: message});
+        }
+    });
+
+};
