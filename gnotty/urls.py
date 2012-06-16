1b7ef39a1f27e8a2e8c53bd536ab9c9b20d321d8
Stephen McDonald <steve@jupo.org>
2012-06-16 17:56:00 +1000

    Added calendar views for messages.

--- gnotty/urls.py
+++ gnotty/urls.py
@@ -2,9 +2,15 @@
 from django.conf.urls.defaults import patterns, url
 
 
+archive_pattern = "^archive/"
+year_pattern    = archive_pattern + "(?P<year>\d{4})/"
+month_pattern   = year_pattern    + "(?P<month>\d{1,2})/"
+day_pattern     = month_pattern   + "(?P<day>\d{1,2})/"
+
 urlpatterns = patterns("gnotty.views",
     url("^$", "chat", name="gnotty_chat"),
-    url("^search/$", "search", name="gnotty_search"),
-    url("^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$",
-        "day", name="gnotty_day"),
+    url(archive_pattern + "$", "messages", name="gnotty_search"),
+    url(year_pattern    + "$", "calendar", name="gnotty_year"),
+    url(month_pattern   + "$", "calendar", name="gnotty_month"),
+    url(day_pattern     + "$", "messages", name="gnotty_day"),
 )
--- gnotty/views.py
+++ gnotty/views.py
@@ -1,9 +1,10 @@
 
-from datetime import date
+from calendar import Calendar, SUNDAY
+from datetime import datetime, date
 
 from django.conf import settings
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from gnotty.models import IRCMessage
 from gnotty.settings import (IRC_HOST, IRC_PORT, IRC_CHANNEL,
@@ -21,20 +22,55 @@ def chat(request, template="gnotty/chat.html"):
     return render(request, template, context)
 
 
-def search(request, template="gnotty/search.html"):
-    messages = []
+def messages(request, year=None, month=None, day=None,
+             template="gnotty/messages.html"):
+    """
+    Show messages for the given query or day.
+    """
+
     query = request.REQUEST.get("q")
     if query:
         search = Q(message__icontains=query) | Q(nickname__icontains=query)
         messages = IRCMessage.objects.filter(search).order_by("-message_time")
+    elif year and month and day:
+        messages = IRCMessage.objects.filter(message_time__year=year,
+                                             message_time__month=month,
+                                             message_time__day=day)
+    else:
+        return redirect("gnotty_year", year=datetime.now().year)
+
     context = {"messages": messages}
     return render(request, template, context)
 
 
-def day(request, year, month, day, template="gnotty/day.html"):
-    day_date = date(year=int(year), month=int(month), day=int(day))
-    messages = IRCMessage.objects.filter(message_time__year=year,
-                                         message_time__month=month,
-                                         message_time__day=day)
-    context = {"messages": messages, "date": day_date}
+def calendar(request, year=None, month=None, template="gnotty/calendar.html"):
+    """
+    Show calendar months for the given year/month.
+    """
+
+    days = [d.date() for d in IRCMessage.objects.dates("message_time", "day")]
+    min_date, max_date = days[0], days[-1]
+    days = set(days)
+    months = []
+    calendar = Calendar(SUNDAY)
+    try:
+        year = int(year)
+    except TypeError:
+        year = datetime.now().year
+
+    for m in range(1, 13) if not month else [int(month)]:
+        lt_max = m <= max_date.month or year < max_date.year
+        gt_min = m >= min_date.month or year > min_date.year
+        if lt_max and gt_min:
+            weeks = calendar.monthdatescalendar(year, m)
+            for w, week in enumerate(weeks):
+                for d, day in enumerate(week):
+                    weeks[w][d] = {
+                        "date": day,
+                        "in_month": day.month == m,
+                        "has_messages": day in days,
+                    }
+            months.append({"month": date(year, m, 1), "weeks": weeks})
+
+    context = {"months": months}
     return render(request, template, context)
