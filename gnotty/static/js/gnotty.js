ea31665485501aa1c34c362b1b88cbf9604e8bd1
Stephen McDonald <steve@jupo.org>
2012-06-18 08:11:49 +1000

    Move UI JavaScript into gnotty.js

--- gnotty/static/js/gnotty.js
+++ gnotty/static/js/gnotty.js
@@ -57,3 +57,93 @@ var IRCClient = function(options) {
     });
 
 };
+
+// UI setup.
+var gnotty = function(options) {
+
+    // Assign a dark colour to each nickname.
+    var colors = {};
+    var color = function(nickname) {
+        if (!colors[nickname]) {
+            var c = function() {
+                return Math.ceil(Math.random() * 150);
+            };
+            colors[nickname] = 'rgb(' + c() + ',' + c() + ',' + c() + ')';
+        }
+        return colors[nickname];
+    };
+
+    // Main setup function called when nickname is entered.
+    // Creates IRC client and sets up event handlers.
+    var start = function(nickname) {
+
+        options.ircNickname = nickname;
+        client = new IRCClient(options);
+
+        // Once connected, show the 'leaves' button, user list,
+        // and change the submit text to 'Send' for sending messages.
+        client.onConnect = function() {
+            $('#input').animate({width: '65%'}, function() {
+                $('.hidden').slideDown(function() {
+                    $('#submit').val('Send');
+                }).removeClass('hidden');
+            });
+        };
+
+        // Render the nickanmes list each time we receive it,
+        // which is each time someone joins or leaves.
+        client.onNicknames = function(nicknames) {
+            nicknames = $.map(nicknames.sort(), function(nickname) {
+                return {nickname: nickname, color: color(nickname)};
+            });
+            var data = {nicknames: nicknames};
+            $('#nicknames').html($('#nicknames-template').tmpl(data));
+        };
+
+        // Add a timestamp to each message as we receive it, and
+        // add it to the messages display. Also scroll the window.
+        client.onMessage = function(data) {
+            var d = new Date();
+            var parts = [d.getHours(), d.getMinutes(), d.getSeconds()];
+            data.time = $.map(parts, function(s) {
+                return (String(s).length == 1 ? '0' : '') + s;
+            }).join(':')
+            data.color = color(data.nickname);
+            $('#messages-template').tmpl(data).appendTo('#messages');
+            window.scrollBy(0, 10000);
+        };
+
+    };
+
+    // Main submit handler - if there are still hidden elements,
+    // we haven't connected yet, so the value submitted is the
+    // initial nickname. Otherwise we've started, and the value
+    // submitted is a message.
+    $('.chat').submit(function() {
+        var value = $('#input').val();
+        if (value) {
+            if ($('.hidden').length > 0) {
+                start(value);
+            } else {
+                client.message(value);
+            }
+        }
+        $('#input').val('').focus();
+        return false;
+    });
+
+    // Leaves just reloads - this will trigger a disconnect for
+    // the client.
+    $('#leave').click(function() {
+        location = location.href.split('?')[0];
+    });
+
+    // Join if there's a nickname in the querystring.
+    var parts = location.href.split('?nickname=');
+    if (parts.length == 2) {
+        start(parts[1].split('&')[0]);
+    }
+
+    $('#input').val('').focus();
+
+};
