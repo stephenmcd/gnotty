3cc4ab4f537b357bc54f71173ac227199c696ae3
Stephen McDonald <steve@jupo.org>
2012-07-22 18:18:39 +1000

    Clean up the chat bot.

--- gnotty/bots.py
+++ gnotty/bots.py
@@ -1,5 +1,7 @@
 
-from random import choice
+from random import choice, randint
+
+from gevent import sleep
 
 from gnotty.client import BaseIRCClient
 
@@ -16,13 +18,20 @@ class BaseBot(BaseIRCClient):
         """
         print "[%(server)s%(channel)s] %(nickname)s: %(message)s" % kwargs
 
-    def log_args(self, event, message):
+    def log_args(self, event, message, nickname=None):
         return {
             "server": self.connection.server,
             "channel": self.channel,
-            "nickname": self.get_nickname(event),
+            "nickname": nickname or self.get_nickname(event),
             "message": message,
         }
+
+    def message_channel(self, message):
+        """
+        We won't receive our own messages, so log them manually.
+        """
+        self.log(**self.log_args(None, message, self.nickname))
+        super(BaseBot, self).message_channel(message)
 
     def on_join(self, connection, event):
         self.log(**self.log_args(event, "joins"))
@@ -50,34 +59,46 @@ class DjangoBot(BaseBot):
 
 
 class ChatMixin(object):
+    """
+    A demo bot mixin that greets and responds to people.
+    """
 
     def __init__(self, *args, **kwargs):
-        super(ChatterMixin, self).__init__(*args, **kwargs)
+        super(ChatMixin, self).__init__(*args, **kwargs)
+        self.chatbots = []
+        self.greetings = ("Hi", "Hello", "Howdy", "Welcome")
         try:
-            from nltk.chat.eliza import eliza_chatbot
-            from nltk.chat.iesha import iesha_chatbot
-            from nltk.chat.rude import rude_chatbot
-            from nltk.chat.suntsu import suntsu_chatbot
-            from nltk.chat.zen import zen_chatbot
+            from nltk.chat import bots
         except ImportError:
-            print "ChatterMixin requires nltk installed"
+            from warnings import warn
+            warn("ChatMixin requires nltk installed")
         else:
-            self.chatbots = [eliza_chatbot, iesha_chatbot, rude_chatbot,
-                             suntsu_chatbot, zen_chatbot]
+            get_bot = lambda x: x[0].func_globals["%sbot" % x[0].__name__
+            self.chatbots = map(get_bot, bots)
+
+    def message_channel_delayed(self, message):
+        """
+        Pause for a random few seconds before messaging, to seem less
+        bot like.
+        """
+        sleep(randint(2, 5))
+        self.message_channel(message)
 
     def on_join(self, connection, event):
+        super(ChatMixin, self).on_join(connection, event)
         nickname = self.get_nickname(event)
-        greetings = ("Hi", "Hello", "Howdy", "Welcome")
+        greeting = choice(self.greetings)
         if nickname != self.nickname:
-            self.message_channel("%s: %s" % (nickname, choice(greetings)))
-        super(GreeterMixin, self).on_join(connection, event)
+            self.message_channel_delayed("%s: %s" % (nickname, greeting))
 
     def on_pubmsg(self, connection, event):
-        from nltk.chat.rude import rude_chatbot
+        super(ChatMixin, self).on_pubmsg(connection, event)
+        if not self.chatbots:
+            return
         for message in event.arguments():
             prefix = "%s: " % self.nickname
             if message.startswith(prefix):
                 nickname = self.get_nickname(event)
                 chatbot = choice(self.chatbots)
                 reply = chatbot.respond(message.replace(prefix, "", 1))
-                self.message_channel("%s: %s" % (nickname, reply))
+                self.message_channel_delayed("%s: %s" % (nickname, reply))
